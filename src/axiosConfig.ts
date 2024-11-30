a4ec6b469a18f6c018334af95f6a2671b0423cee
Gustavo.Batista <gustavo.batista@libresolucoes.com.br>
2024-11-29 23:24:38 -0300

    fix: corrigindo instancia do axios

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -17,6 +17,8 @@ api.interceptors.request.use(
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
+    
+    config.withCredentials = true; 
     return config;
   },
   (error) => {
