701a40f93971b5f447cc81ebd6f52d84689f872f
Gustavo.Batista <gustavo.batista@libresolucoes.com.br>
2024-11-30 11:38:35 -0300

    fix

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -17,8 +17,8 @@ api.interceptors.request.use(
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
-    
-    config.withCredentials = true; 
+
+    config.withCredentials = true;
     return config;
   },
   (error) => {
