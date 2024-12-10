f408a1880a438488e16df3806f4aa972d5342c78
Gustavo.Batista <gustavo.batista@libresolucoes.com.br>
2024-12-10 20:13:45 -0300

    fix

--- src/pages/ScheduleClient.tsx
+++ src/pages/ScheduleClient.tsx
@@ -311,7 +311,7 @@ export const ScheduleClient = () => {
         <Stack spacing={4} as="form" onSubmit={handleSubmit(onSubmit)}>
           {currentStep === 1 && (
             <>
-              <Box>
+              <Box marginTop="32px">
                 <>
                   <Box margin="16px 0">
                     <CustomInput
